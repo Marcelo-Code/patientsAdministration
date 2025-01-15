3bf567033b4e7eca7fa959018255b19548636555
Marcelo <feltesmarcelo@gmail.com>
2025-01-15 15:02:02 -0300

    commit

--- frontend/src/components/layout/navBar/NavBarContainer.jsx
+++ frontend/src/components/layout/navBar/NavBarContainer.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { ConfirmAlert } from "../../common/alerts/alerts";
 import { getPatientsRecords } from "../../../api/pacientes/patients";
 import { getProfessionalsRecords } from "../../../api/profesionales/professionals";
+import { Spinner } from "../../common/spinner/Spinner";
 
 export const NavBarContainer = () => {
   const [patientsRecords, setPatientsRecords] = useState(null);
@@ -27,6 +28,9 @@ export const NavBarContainer = () => {
       })
       .catch((error) => console.log(error));
   }, [updateAlertsList]);
+
+  if (!patientsRecords || !professionalsRecords || !userRolRecord)
+    return <Spinner />;
 
   const professionalsExpirationRnpRecords = (professionalsRecords || [])
     .map((record) => {
